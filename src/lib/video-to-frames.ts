77c163eb002185f53bba7272762f17ea5625181b
Daniel Gaievskyi <gaievskyi@proton.me>
2024-05-03 17:31:18 +0200

    ⬆️ (package.json): remove "nanoid" dependency and upgrade other dependencies ♻️ (video-to-frames.ts): refactor VideoToFrames class to use OffscreenCanvas and improve code readability and performance

--- src/lib/video-to-frames.ts
+++ src/lib/video-to-frames.ts
@@ -1,5 +1,3 @@
-import { nanoid } from "nanoid"
-
 export enum VideoToFramesMethod {
   fps,
   totalFrames,
@@ -8,42 +6,36 @@ export enum VideoToFramesMethod {
 export type Frame = { id: string; src: string }
 
 export class VideoToFrames {
-  /**
-   * Extracts frames from the video and returns them as an array of imageData
-   * @param videoUrl url to the video file (html5 compatible format) eg: mp4
-   * @param amount number of frames per second or total number of frames that you want to extract
-   * @param type [fps, totalFrames] The method of extracting frames: Number of frames per second of video or the total number of frames acros the whole video duration. defaults to fps
-   */
   public static getFrames(
     videoUrl: string,
     amount: number,
     type: VideoToFramesMethod = VideoToFramesMethod.fps,
   ): Promise<Frame[]> {
-    return new Promise((resolve: (frames: Frame[]) => void) => {
-      const frames: { id: string; src: string }[] = []
-      const canvas: HTMLCanvasElement = document.createElement("canvas")
+    return new Promise((resolve) => {
+      const frames: Frame[] = []
+      const canvas = new OffscreenCanvas(0, 0)
       const context = canvas.getContext("2d")!
       let duration = 0
 
       const video = document.createElement("video")
       video.preload = "auto"
-      let that = this
 
-      video.addEventListener("loadeddata", async function () {
+      video.addEventListener("loadeddata", async () => {
         canvas.width = video.videoWidth
         canvas.height = video.videoHeight
         duration = video.duration
-        let totalFrames = amount
-        if (type === VideoToFramesMethod.fps) {
-          totalFrames = duration * amount
-        }
+        const totalFrames =
+          type === VideoToFramesMethod.fps ? duration * amount : amount
+
         for (let time = 0; time < duration; time += duration / totalFrames) {
-          const id = nanoid()
-          const src = await that.getVideoFrame(video, context, canvas, time)
+          const id = time.toString()
+          const src = await this.getVideoFrame(video, context, canvas, time)
           frames.push({ id, src })
         }
+
         resolve(frames)
       })
+
       video.src = videoUrl
       video.load()
     })
@@ -51,36 +43,35 @@ export class VideoToFrames {
 
   private static getVideoFrame(
     video: HTMLVideoElement,
-    context: CanvasRenderingContext2D,
-    canvas: HTMLCanvasElement,
+    context: OffscreenCanvasRenderingContext2D,
+    canvas: OffscreenCanvas,
     time: number,
   ): Promise<string> {
-    return new Promise(
-      (resolve: (frame: string) => void, reject: (error: string) => void) => {
-        const eventCallback = () => {
-          video.removeEventListener("seeked", eventCallback)
-          this.storeFrame(video, context, canvas, resolve, reject)
-        }
-        video.addEventListener("seeked", eventCallback)
-        video.currentTime = time
-      },
-    )
+    return new Promise((resolve, reject) => {
+      const onSeeked = () => {
+        video.removeEventListener("seeked", onSeeked)
+        this.storeFrame(video, context, canvas, resolve, reject)
+      }
+      video.addEventListener("seeked", onSeeked)
+      video.currentTime = time
+    })
   }
 
   private static storeFrame(
     video: HTMLVideoElement,
-    context: CanvasRenderingContext2D,
-    canvas: HTMLCanvasElement,
+    context: OffscreenCanvasRenderingContext2D,
+    canvas: OffscreenCanvas,
     resolve: (frame: string) => void,
     reject: (error: string) => void,
   ) {
-    try {
-      context.drawImage(video, 0, 0, video.videoWidth, video.videoHeight)
-      resolve(canvas.toDataURL())
-    } catch (error) {
-      if (error instanceof Error) {
-        reject(error.message)
-      }
-    }
+    context.drawImage(video, 0, 0, video.videoWidth, video.videoHeight)
+    canvas.convertToBlob().then(
+      (blob) => {
+        const reader = new FileReader()
+        reader.onloadend = () => resolve(reader.result?.toString() ?? "")
+        reader.readAsDataURL(blob)
+      },
+      (error) => reject(error.message),
+    )
   }
 }
