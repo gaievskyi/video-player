4a9e8a2c4bae40e5b6a1e785bf2331e1b3dede39
Daniel Gaievskyi <gaievskyi@proton.me>
2024-05-03 17:13:46 +0200

    📝 (video-upload-input.tsx): remove unnecessary text color classes to simplify styling and improve readability

--- src/components/video-upload-input.tsx
+++ src/components/video-upload-input.tsx
@@ -10,7 +10,7 @@ export const VideoUploadInput = ({ onChange }: VideoUploadInputProps) => {
     >
       <div className="flex flex-col items-center justify-center pb-6 pt-5">
         <svg
-          className="mb-4 h-8 w-8 text-gray-500 dark:text-gray-400"
+          className="mb-4 h-8 w-8"
           aria-hidden="true"
           xmlns="http://www.w3.org/2000/svg"
           fill="none"
@@ -24,12 +24,8 @@ export const VideoUploadInput = ({ onChange }: VideoUploadInputProps) => {
             d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"
           />
         </svg>
-        <p className="mb-1 text-sm font-bold text-gray-500 dark:text-gray-400">
-          Open video
-        </p>
-        <p className="text-xs text-gray-500 dark:text-gray-400">
-          .mp4, .webm, or .ogg
-        </p>
+        <p className="mb-1 text-sm font-bold">Open video</p>
+        <p className="text-xs">.mp4, .webm, or .ogg</p>
       </div>
       <input
         id="dropzone-file"
